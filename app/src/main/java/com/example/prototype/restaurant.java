1a277b8f4748f7109d93cff6c5a6e665273cd20a
Albert Truong <albertoe4788@gmail.com>
2021-01-21 18:35:30 -0800

    Added toolbar to restaurant with text and icons

--- app/src/main/java/com/example/prototype/restaurant.java
+++ app/src/main/java/com/example/prototype/restaurant.java
@@ -1,6 +1,7 @@
 package com.example.prototype;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class restaurant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar );
+        setSupportActionBar(myToolbar);
+
     }
 }
