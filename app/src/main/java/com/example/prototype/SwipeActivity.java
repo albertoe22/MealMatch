c7f7bd610457c0fdf6c0ed5e56fe0f75c7c3f4c7
eseng <30567474+eseng@users.noreply.github.com>
2021-02-04 18:26:08 -0800

    added buttons for matches, settings, swipe screens

--- app/src/main/java/com/example/prototype/SwipeActivity.java
+++ app/src/main/java/com/example/prototype/SwipeActivity.java
@@ -3,7 +3,9 @@ package com.example.prototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SwipeActivity extends AppCompatActivity {
 
@@ -11,5 +13,13 @@ public class SwipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
+    }
+
+    public void toMatches(View view) {
+        startActivity(new Intent(this, Matches.class));
+    }
+
+    public void toSettings(View view) {
+        startActivity(new Intent(this, settings.class));
     }
 }
--- app/src/main/java/com/example/prototype/settings.java
+++ app/src/main/java/com/example/prototype/settings.java
@@ -2,7 +2,9 @@ package com.example.prototype;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class settings extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
+
+
+    public void toSwipe(View view) {
+        startActivity(new Intent(this, SwipeActivity.class));
+    }
+
 }
