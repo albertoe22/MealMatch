98686993a019f8da8732fd7621c66620f5bf0845
richard-pho <richard.pho884@gmail.com>
2021-02-04 18:36:49 -0800

    new card xml and some more stuff for swiping

--- app/src/main/java/com/example/prototype/Stack.java
+++ app/src/main/java/com/example/prototype/Stack.java
@@ -1,4 +1,42 @@
 package com.example.prototype;
 
-public class Stack {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Stack extends RecyclerView.Adapter<Stack.ViewHolder> {
+    private List<Card> cards;
+    public Stack(List<Card> c){
+        cards = c;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.card,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cards.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
