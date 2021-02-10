1384f124c10e6fad21ede6a2bd97a7772dc7cbf5
albertoe22 <38597481+albertoe22@users.noreply.github.com>
2021-02-09 19:06:24 -0800

    fix to the gradle build file

--- app/src/main/java/com/example/prototype/SwipeActivity.java
+++ app/src/main/java/com/example/prototype/SwipeActivity.java
@@ -7,6 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.libraries.places.api.Places;
+
+import com.google.android.libraries.places.api.net.PlacesClient;
+
 public class SwipeActivity extends AppCompatActivity {
 
     @Override
@@ -14,6 +18,8 @@ public class SwipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
     }
+    //Places.initialize(getApplicationContext(), apiKey);
+    PlacesClient placesClient = Places.createClient(this);
 
     public void toMatches(View view) {
         startActivity(new Intent(this, Matches.class));
