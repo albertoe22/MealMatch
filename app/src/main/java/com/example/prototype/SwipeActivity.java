653f8c59d517af1e48054ec8713e32c829de7da5
richard-pho <richard.pho884@gmail.com>
2021-02-22 22:42:23 -0800

    swipe working

--- app/src/main/java/com/example/prototype/SwipeActivity.java
+++ app/src/main/java/com/example/prototype/SwipeActivity.java
@@ -5,20 +5,84 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.google.android.libraries.places.api.Places;
 
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+
+import java.util.ArrayList;
 
 public class SwipeActivity extends AppCompatActivity {
     String apiKey = "AIzaSyDgMhZAjvbssW3MFNWJ5yTgoJkLj2PHQuc";
+    private ArrayList<String> example;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe);
         Places.initialize(getApplicationContext(), apiKey);
         PlacesClient placesClient = Places.createClient(this);
+        example = new ArrayList<>();
+        example.add("Chipotle");
+        example.add("Chickfila");
+        example.add("Wendys");
+        example.add("Mcdonalds");
+        example.add("BurgerKing");
+        example.add("KrustyKrab");
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.card, R.id.test, example );
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.cards);
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                example.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(SwipeActivity.this,"left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(SwipeActivity.this,"right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                example.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(SwipeActivity.this,"click", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void toMatches(View view) {
