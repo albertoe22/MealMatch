c6c5eb26d55dfac002270b5d7914229e4f22ec6f
eseng <30567474+eseng@users.noreply.github.com>
2021-03-11 18:32:57 -0800

    signout

--- app/src/main/java/com/example/prototype/settings.java
+++ app/src/main/java/com/example/prototype/settings.java
@@ -6,6 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class settings extends AppCompatActivity {
 
     @Override
@@ -14,6 +20,11 @@ public class settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
+
+    public void signOut(View v) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this, login.class));
+    }
 
     public void toSwipe(View view) {
         startActivity(new Intent(this, SwipeActivity.class));
