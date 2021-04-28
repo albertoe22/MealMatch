cbdecac0df6b3ae6c82948c5e9fd8fceb381ed7e
eseng <30567474+eseng@users.noreply.github.com>
2021-04-27 19:03:48 -0700

    matches menu on click

--- app/src/main/java/com/example/prototype/MatchesViewHolders.java
+++ app/src/main/java/com/example/prototype/MatchesViewHolders.java
@@ -1,5 +1,7 @@
 package com.example.prototype;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
     public TextView mMatchAddress, mMatchName;
     public ImageView mMatchImage;
+    public String placeId;
     public MatchesViewHolders (View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
@@ -19,6 +22,10 @@ public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.
     }
     @Override
     public void onClick(View v) {
-
+        String url = "https://www.google.com/maps/search/?api=1&query=...&query_place_id=...";
+        Uri uri = Uri.parse(url);
+        //https://www.google.com/maps/search/?api=1&query=47.5951518,-122.3316393&query_place_id=ChIJKxjxuaNqkFQR3CK6O1HNNqY
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        v.getContext().startActivity(intent);
     }
 }
