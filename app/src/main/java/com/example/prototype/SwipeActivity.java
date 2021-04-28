b3ddd0278a43295c354fc313178e378b3e3e9079
eseng <30567474+eseng@users.noreply.github.com>
2021-04-27 17:47:36 -0700

    updated settings / empty restaurant notification

--- app/src/main/java/com/example/prototype/SwipeActivity.java
+++ app/src/main/java/com/example/prototype/SwipeActivity.java
@@ -185,8 +185,11 @@ public class SwipeActivity extends AppCompatActivity {
                         public void onAdapterAboutToEmpty(int itemsInAdapter) {
                             // Ask for more data here
                             //rowItems.add("XML ".concat(String.valueOf(i)));
-                            if (itemsInAdapter <= 4) {
-                                Toast.makeText(SwipeActivity.this,"Almost out of restaurants increase distance in settings or reset matches", Toast.LENGTH_SHORT).show();
+                            if (itemsInAdapter == 0) {
+                                Toast.makeText(SwipeActivity.this,"Out of restaurants, increase distance in settings or reset matches", Toast.LENGTH_SHORT).show();
+                            }
+                            else if (itemsInAdapter <= 4) {
+                                Toast.makeText(SwipeActivity.this,"Almost out of restaurants, increase distance in settings or reset matches", Toast.LENGTH_SHORT).show();
                             }
                             arrayAdapter.notifyDataSetChanged();
                             Log.d("LIST", "notified");
