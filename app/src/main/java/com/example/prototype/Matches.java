fb7d59b14de3d5077cca9ca6ffaf01efde505a98
eseng <30567474+eseng@users.noreply.github.com>
2021-04-29 17:37:45 -0700

    matches to settings button

--- app/src/main/java/com/example/prototype/Matches.java
+++ app/src/main/java/com/example/prototype/Matches.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -109,6 +111,10 @@ public class Matches extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void toSettings(View view) {
+        startActivity(new Intent(this, settings.class));
     }
 
 
